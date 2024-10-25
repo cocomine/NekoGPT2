30e46a19167b7ae840ce57793b00ea3b8de333ee
cocomine <roguish.6888467468@gmail.com>
2024-10-26 02:52:59 +0800

    deploy
    Signed-off-by: cocomine <roguish.6888467468@gmail.com>

--- src/DatabaseHelper.py
+++ src/DatabaseHelper.py
@@ -15,19 +15,18 @@ def database_helper(db: Connection | PooledMySQLConnection | MySQLConnectionAbst
     :param db: database connection
     :param bot_name: bot name
     """
-    cursor = cursor_wrapper(db)
 
     # Initialize database
     def initializing():
         # Initialize database
+        cursor2 = cursor_wrapper(db)
         logging.info(f"{bot_name} Initializing database...")
-        cursor = cursor_wrapper(db)
 
-        cursor.execute(
+        cursor2.execute(
             "create table if not exists Guild(Guild_ID varchar(25) not null primary key, replyAt tinyint(1) default 1 not null)", [])
-        cursor.execute(
+        cursor2.execute(
             "create table if not exists DM(User varchar(25) not null primary key, conversation varchar(35) not null)", [])
-        cursor.execute("""create table if not exists ReplyAt(
+        cursor2.execute("""create table if not exists ReplyAt(
                     Guild_ID     varchar(25)             not null,
                     user         varchar(25)             not null,
                     conversation varchar(35)             null,
@@ -36,7 +35,7 @@ def database_helper(db: Connection | PooledMySQLConnection | MySQLConnectionAbst
                         foreign key (Guild_ID) references Guild (Guild_ID)
                             on update cascade on delete cascade
                 )""", [])
-        cursor.execute("""create table if not exists ReplyThis(
+        cursor2.execute("""create table if not exists ReplyThis(
                     Guild_ID     varchar(25)             not null,
                     channel_ID   varchar(25)             not null,
                     conversation varchar(35)             null,
@@ -45,28 +44,27 @@ def database_helper(db: Connection | PooledMySQLConnection | MySQLConnectionAbst
                         foreign key (Guild_ID) references Guild (Guild_ID)
                             on update cascade on delete cascade
                 )""", [])
-        cursor.execute(
+        cursor2.execute(
             "CREATE TABLE if not exists `setting` (`key` varchar(20) NOT NULL, `value` TEXT NOT NULL, PRIMARY KEY(`key`))", [])
-        cursor.execute("INSERT INTO setting (`key`, `value`) VALUES ('version', '0.4')", [])
+        cursor2.execute("INSERT INTO setting (`key`, `value`) VALUES ('version', '0.4')", [])
         db.commit()
 
         logging.info(f"{bot_name} Database initialized.")
 
     try:
+        cursor = cursor_wrapper(db)
         cursor.execute("SELECT value FROM setting WHERE `key` = 'version'") # check database version sql
+
+        # check database version
+        version = cursor.fetchone()
+        if version is not None:
+            if version[0] == "0.4":
+                logging.info(f"{bot_name} Database is up to date.")
+                return
     except mysql.connector.Error:
         initializing()
     except sqlite3.Error:
         initializing()
-    finally:
-        db.commit()
-
-    # check database version
-    version = cursor.fetchone()
-    if version is not None:
-        if version[0] == "0.4":
-            logging.info(f"{bot_name} Database is up to date.")
-            return
 
     # recheck update
     database_helper(db, bot_name)
